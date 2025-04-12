bc98b2151fb273e52b087df1fac83e511865da21
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-04-12 16:25:56 -0400

    Aplicando encapsulamiento

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,5 +3,27 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
 
+    int getTotalDeLasEvaluaciones() {
+        return totalDeLasEvaluaciones;
+    }
+
+    void muestraFichaTecnica() {
+        System.out.println("El nombre de la pelicula es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos :" + duracionEnMinutos);
+
+    }
+
+    void evalua(double nota) {
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia (){
+        return sumaDeLasEvaluaciones/totalDeLasEvaluaciones;
+    }
 }
+
--- src/Principal.java
+++ src/Principal.java
@@ -5,8 +5,21 @@ public class Principal {
         miPelicula.fechaDeLanzamiento = 2021;
         miPelicula.duracionEnMinutos = 120;
 
-        System.out.println("Mi pelicula es: " + miPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento es: " + miPelicula.fechaDeLanzamiento);
+        miPelicula.muestraFichaTecnica();
+
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionEnMinutos = 180;
+
+        //otraPelicula.muestraFichaTecnica();
 
     }
 
